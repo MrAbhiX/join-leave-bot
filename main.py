885c881258de7660327d866cc9743e4dcc977225
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-16 14:47:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ Abhi = Client(os.environ["SESSION_NAME"],
 
 
 
+bot = Client(
+    "userbot Join-Leave Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+Abhi.start()
+bot.run()
