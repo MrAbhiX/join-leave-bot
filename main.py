0d7c40caa7477af58617a3a4b437c637a613d872
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-29 09:41:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ bot = Client(
     "userbot Join-Leave Bot",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"]
+    api_hash = os.environ["API_HASH"],
+    plugins=dict(root="Userbot"),
 )
 
 Abhi.start()
